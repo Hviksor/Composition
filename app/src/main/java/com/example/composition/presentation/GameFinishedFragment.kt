4c92c1706e731dc79af58f73e55cbdde4c518cfb
eisrkov <vsvktor@gmail.com>
2022-07-29 18:03:13 +0300

    3

---
+++ app/src/main/java/com/example/composition/presentation/GameFinishedFragment.kt
@@ -0,0 +1,14 @@
+package com.example.composition.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.composition.R
+
+class GameFinishedFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return layoutInflater.inflate(R.layout.fragment_game_finished, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/composition/presentation/GameFragment.kt
@@ -0,0 +1,15 @@
+package com.example.composition.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.composition.R
+
+class GameFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return layoutInflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+}
